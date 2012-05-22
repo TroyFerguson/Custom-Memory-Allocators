2b14b4f038fe42f529b913cf94e9dc0bc602d8ee
Troy Ferguson <troy.a.ferguson@gmail.com>
2012-05-22 21:33:41 +1000

    Started working on the memory handle class

---
+++ Custom-Memory-Allocators/CustomAllocatorTest2/MemoryHandle.cpp
@@ -0,0 +1,9 @@
+#include "MemTypes.h"
+#include "Assertion.h"
+#include "MemoryHandle.h"
+
+template <class T>
+MemoryHandle< T >::MemoryHandle()
+{
+
+}
--- Custom-Memory-Allocators/CustomAllocatorTest2/MemoryHandle.h
+++ Custom-Memory-Allocators/CustomAllocatorTest2/MemoryHandle.h
@@ -3,11 +3,43 @@
 
 #include "MemTypes.h"
 
+template <class T>
 class MemoryHandle
 {
+public:
+
+	MemoryHandle();
+	MemoryHandle( void* );
+
+	void operator=( const void* );
+	T& operator*();
+	const T& operator*() const;
+	T* operator->();
+	const T* operator->() const;
+
 private:
 	U32 HandleIndex;
 	//TODO: add in ID to prevent stale object issue
 };
 
 #endif
+
+//T* operator->() {
+//        return ptr;
+//    }
+//
+//    // const version, returns a pointer-to-const instead of just a pointer to
+//    // enforce the idea of the logical constness of this object 
+//    const T* operator->() const {
+//        return ptr;
+//    }
+//
+//    T& operator*() {
+//        return *ptr;
+//    }
+//
+//    // const version, returns a const reference instead of just a reference
+//    // to enforce the idea of the logical constness of this object
+//    const T& operator*() const {
+//        return *ptr;
+//    }
